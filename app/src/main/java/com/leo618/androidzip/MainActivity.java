170abdef1f51690248a465835f8ceeb60f012eb8
RenCho Hwang <hjhrq1991@gmail.com>
2025-06-14 18:24:16 +0800

    1.更新示例代码

--- app/src/main/java/com/leo618/androidzip/MainActivity.java
+++ app/src/main/java/com/leo618/androidzip/MainActivity.java
@@ -2,19 +2,21 @@ package com.leo618.androidzip;
 
 import android.Manifest;
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
-import com.leo618.splashpermissionsauth.SplashAuthUI;
+import com.hjq.permissions.OnPermissionCallback;
+import com.hjq.permissions.XXPermissions;
 import com.leo618.zip.IZipCallback;
 import com.leo618.zip.ZipManager;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * function:压缩解压测试
@@ -40,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void zip(View view) {
         if (!authPermission()) return;
+
         ArrayList<File> files = new ArrayList<>();
         files.add(file1);
         files.add(file2);
@@ -116,15 +119,23 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean authPermission() {
         if (hasPermission) return true;
-        SplashAuthUI.launch(MainActivity.this, 100, new String[]{
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-        });
-        return false;
-    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        hasPermission = (requestCode == 100) && (resultCode == RESULT_OK);
+        XXPermissions.with(MainActivity.this).permission(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                .unchecked()
+                .request(new OnPermissionCallback(){
+                    @Override
+                    public void onGranted(@NonNull List<String> permissions, boolean allGranted) {
+                        hasPermission = allGranted;
+                    }
+
+                    @Override
+                    public void onDenied(@NonNull List<String> permissions, boolean doNotAskAgain) {
+                        OnPermissionCallback.super.onDenied(permissions, doNotAskAgain);
+                    }
+                });
+
+        return false;
     }
 }
